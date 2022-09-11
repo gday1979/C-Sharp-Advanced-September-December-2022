b0d403e09986fd836b91ddfda264fe278e35fa21
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-09-11 16:00:55 +0300

    commit

---
+++ 8. Traffic Jam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
