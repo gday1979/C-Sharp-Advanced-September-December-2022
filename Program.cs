f68fdb41e26dc7618ecb5f33749d1303782bc80f
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-09-11 15:07:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_Advanced_September_December_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
