97a9b76dbb66b34162deb9e2e89269c6dab3450d
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-09-21 20:41:36 +0300

    commit

--- 01. Count Same Values in Arrayy/Program.cs
+++ 01. Count Same Values in Arrayy/Program.cs
@@ -6,7 +6,7 @@ namespace _01._Count_Same_Values_in_Arrayy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
