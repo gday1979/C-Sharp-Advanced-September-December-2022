80b0bc6f7dc5537d9f4b2ed5318e22d0cde7f98e
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-09-25 15:52:10 +0300

    commit

---
+++ 01. Even Lines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Even_Lines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
