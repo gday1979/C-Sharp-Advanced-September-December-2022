1eed65951f062f58ee1ab188e57a4c9c749b46f5
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-10-01 10:02:32 +0300

    commit

--- 02. Car Extension/Program.cs
+++ 02. Car Extension/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace _02._Car_Extension
+
+namespace CarManufacturer
 {
-    internal class Program
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
         }
     }
 }
