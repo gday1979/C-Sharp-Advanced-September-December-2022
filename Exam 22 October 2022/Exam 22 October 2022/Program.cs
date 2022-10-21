e242d830c84f9bffb6959c3a895033ec149fe4ee
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-10-21 20:21:21 +0300

    update

---
+++ Exam 22 October 2022/Exam 22 October 2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exam_22_October_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
